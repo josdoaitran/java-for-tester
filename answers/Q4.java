edcebf874eb381fd57a726bf156b8c97fa74a230
Doai Tran <josdoaitran@gmail.com>
2018-07-07 18:00:59 +0700

    Update Q4

--- answers/Q4.java
+++ answers/Q4.java
@@ -2,29 +2,19 @@
 /*
  * Print square pattern using nested-loop
  */
-public class SquareBoard {   // Save as "SquareBoard.java"
+public class SquareBoard {
    public static void main (String[] args) {
       int size = 5;    // size of the board
-      for (int row = 1; row <= size; ++row) {  // Use row and col, NOT i and j, or x and y
-         for (int col = 1; ......; ......) {
-            ......
+      for (int row = 1; row <= size; ++row) {  
+// Use row and col, NOT i and j, or x and y
+         for (int col = 1; col <= size; ++col) {
+            System.out.print(“#”);
          }
-         ......
+         System.out.println(“”);
       }
    }
 }
 
-// Notes: The code pattern for printing 2D patterns using nested loop is:
 
 
-// Outer loop to print each of the rows
-for (int row = 1; row <= size; row++) {
-   // Inner loop to print each of the columns of one particular row
-   for (int col = 1; col <= size; col++) {
-      System.out.print( ...... )
-      ......
-   }
-   // Print a newline after all the columns
-   System.out.println();
-}
-// You should name the variables row and col, NOT i and j or x and y!!!
+
