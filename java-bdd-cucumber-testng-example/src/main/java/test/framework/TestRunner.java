9237c39f009a70525ce0d9085ed3926a316cf34a
josdoaitran <josdoaitran@gmail.com>
2024-03-25 22:26:29 +0700

    update test push kibana and ìnluxdb

--- java-bdd-cucumber-testng-example/src/main/java/test/framework/TestRunner.java
+++ java-bdd-cucumber-testng-example/src/main/java/test/framework/TestRunner.java
@@ -2,9 +2,14 @@ package test.framework;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.influxdb.dto.Point;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
 import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
 
 @CucumberOptions(plugin = {TestConfiguration.Plugin.PRETTY, TestConfiguration.Plugin.JSON},
         glue = {TestConfiguration.Glue.STEPS})
@@ -16,5 +21,19 @@ public class TestRunner extends AbstractTestNGCucumberTests {
         LOGGER.info("Setup testing for each execution time");
     }
 
+    private void postTestMethodStatus(ITestResult iTestResult, String status) {
+        Point point = Point.measurement("testmethod").time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .tag("testclass", iTestResult.getTestClass().getName()).tag("name", iTestResult.getName())
+                .tag("description", iTestResult.getMethod().getDescription()).tag("result", status)
+                .addField("duration", (iTestResult.getEndMillis() - iTestResult.getStartMillis())).build();
+        UpdateResults.post(point);
+    }
+    private void postTestClassStatus(ITestContext iTestContext) {
+        Point point = Point.measurement("testclass").time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .tag("name", iTestContext.getAllTestMethods()[0].getTestClass().getName())
+                .addField("duration", (iTestContext.getEndDate().getTime() - iTestContext.getStartDate().getTime()))
+                .build();
+        UpdateResults.post(point);
+    }
 }
 
