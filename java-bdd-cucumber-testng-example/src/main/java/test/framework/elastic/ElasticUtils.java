c0ec7cbf440da261e1a3f4d794f212e0111d5aa4
josdoaitran <josdoaitran@gmail.com>
2024-03-26 13:34:32 +0700

    init example

--- java-bdd-cucumber-testng-example/src/main/java/test/framework/elastic/ElasticUtils.java
+++ java-bdd-cucumber-testng-example/src/main/java/test/framework/elastic/ElasticUtils.java
@@ -1,11 +1,13 @@
 package test.framework.elastic;
 
 import io.cucumber.plugin.event.Result;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
-import test.framework.TestRunner;
 import test.framework.utils.PicoContainer;
 
 import java.time.LocalDateTime;
@@ -23,18 +25,22 @@ public class ElasticUtils {
 
         if (System.getProperty("pushToElastic").equalsIgnoreCase("yes")) {
             LOGGER.info("Results to ELastic");
-            Map<String, Object> map = new HashMap<>();
-            map.put("feature", picoContainer.dataStore.get("featureName"));
-            map.put("scenarioName", picoContainer.dataStore.get("scenarioName"));
-            map.put("status", status);
-            map.put("executionTime", LocalDateTime.now().toString());
-            map.put("duration", picoContainer.dataStore.get("duration"));
-            map.put("Test Case ID", "TC-11"); // Can use this to filter the results in Kibana
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("feature", picoContainer.dataStore.get("featureName"));
+            jsonBody.put("scenarioName", picoContainer.dataStore.get("scenarioName"));
+            jsonBody.put("status", status);
+            jsonBody.put("executionTime", LocalDateTime.now().toString());
+            jsonBody.put("duration", picoContainer.dataStore.get("duration"));
+            jsonBody.put("Test Case ID", "TC-11"); // Can use this to filter the results in Kibana
             if (result.getStatus().toString().equals("FAILED")) {
-                map.put("logs", picoContainer.dataStore.get("error"));
+                jsonBody.put("logs", picoContainer.dataStore.get("error"));
             }
-            Response response = given().header("Content-Type", "application/json")
-                    .log().all().body(map.toString())
+
+            Response response = RestAssured.given()
+                    .contentType(ContentType.JSON)
+                    .body(jsonBody.toString())
+                    .log().all()
+                    .when()
                     .post(ElasticConfiguration.ELK_URL);
 
             Assert.assertEquals(response.statusCode(), 201);
